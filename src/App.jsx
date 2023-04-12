e5581e86e05a70b1cc31c1b276b9af1bbf68cb30
= <dcnation1000000@gmail.com>
2023-04-11 22:39:53 -0400

    finish beta

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { useAuth } from "reactfire"
-import Login from "./routes/login"
-import Register from "./routes/register"
-import Chat from "./routes/chat"
-import Profile from "./routes/profile"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "reactfire";
+import Login from "./routes/login";
+import Register from "./routes/register";
+import Chat from "./routes/chat";
+import Profile from "./routes/profile";
+import { useEffect, useState } from "react";
 
 function App() {
+  const auth = useAuth();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const auth = useAuth()
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setIsLoggedIn(Boolean(user));
+      setIsLoading(false);
+    });
 
-  return(
-    <BrowserRouter>
+    return unsubscribe;
+  }, [auth]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/chat" element={<Chat/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/chat"
+          element={isLoggedIn ? <Chat /> : <Navigate to="/register" replace={true} />}
+        />
+        <Route
+          path="/profile"
+          element={isLoggedIn ? <Profile /> : <Navigate to="/register" replace={true} />}
+        />
       </Routes>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
